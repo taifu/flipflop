3176104552f405962f56dbb9996e9afa136fb093
Marco Beri <marcoberi@gmail.com>
2025-12-18 18:50:25 +0100

    Day 7

---
+++ 2025/grids.py
@@ -0,0 +1,35 @@
+import math
+
+
+data = """2 2
+3 3
+2 3
+"""
+data = open("grids.txt").read()
+
+data = data.strip().splitlines()
+
+def reset(data):
+    grids = {}
+    for n, line in enumerate(data):
+        grids[n] = tuple(int(p) for p in line.split())
+    return grids
+grids = reset(data)
+
+tot = 0
+for x, y in grids.values():
+    tot += math.comb(x + y - 2, y - 1)
+print(tot)
+
+tot = 0
+for x, y in grids.values():
+    tot += math.factorial(2 * x + y - 3) // (math.factorial(x  - 1)**2 * math.factorial(y  - 1))
+print(tot)
+
+tot = 0
+for n_dim, size in grids.values():
+    total_steps = (size - 1)*n_dim
+    numerator = math.factorial(total_steps)
+    denominator = math.factorial(size - 1)**n_dim
+    tot += numerator // denominator
+print(tot)
